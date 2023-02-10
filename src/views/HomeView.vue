2f8413c736701af94200bf41a1cdf01acbe9d22d
921857781@qq.com <921857781@qq.com>
2023-02-10 18:17:13 +0800

    'init'

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div style="height: 40px; margin-top: 5px">
+    <el-row style="line-height: 40px; text-align: center">
+      <el-col :span="2" style="text-align: center">
+        <el-button type="danger" round> 文章管理 </el-button>
+      </el-col>
+      <el-col :span="18" style="text-align: center">
+        <el-input @blur="title=$event.target.value.trim()" class='title-ipt' v-model="title" placeholder="请输入标题" />
+      </el-col>
+      <el-col :span="4" style="text-align: center">
+        <el-button round> 保存草靠 </el-button>
+        <el-button type="danger" round> 保存文章 </el-button>
+      </el-col>
+    </el-row>
+
+    <div>
+
+    </div>
+
+    <!-- <el-page-header :icon="ArrowLeft">
+    <template #content>
+      <span class="text-large font-600 mr-3"> Title </span>
+    </template>
+  </el-page-header> -->
+
   </div>
+  <br>
+  <md-editor v-model="text" preview-theme="github" style="height: 800px" :table-shape="[8, 8]" showCodeRowNumber />
+  <el-button> 保存 </el-button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { reactive, ref } from 'vue'
+import MdEditor from 'md-editor-v3'
 
-export default defineComponent({
+export default {
   name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+  components: { MdEditor },
+
+  setup() {
+    let text = ref(`
+\`\`\` python
+a = 10
+print('hello world')
+\`\`\`
+    `)
+    let title = ref('')
+    return { text, title }
+  }
+}
 </script>
+
+<style>
+.title-ipt {
+  width: 100%;
+}
+</style>
